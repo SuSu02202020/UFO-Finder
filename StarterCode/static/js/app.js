cb2e8f681e0391102e2b30213e43de8bd7c4d938
Gata Kass <sueyahara@yahoo.com>
2018-11-08 15:09:21 -0500

    if condition

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,20 +12,29 @@ data.forEach((ufoReport) => {
 });  
 
 var submit = d3.select("#filter-btn");
+
 submit.on("click", function() {
-    d3.event.preventDefault();
+  d3.event.preventDefault();
     var inputDate = d3.select("#datetime");
     var inputValue = inputDate.property("value");
-    var filteredData = tableData.filter(ufoReport => ufoReport.datetime === inputValue);
-    console.log(filteredData);
-
-    tbody.html("");
-
-    filteredData.forEach((filteredUfoReport) => {
-      var row = tbody.append("tr");
-      Object.entries(filteredUfoReport).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });  
-  });
+    
+      var filteredData = tableData.filter(ufoReport => ufoReport.datetime === inputValue);
+      console.log(filteredData);
+  
+      if(ufoReport => ufoReport.datetime === inputValue){
+        tbody.html("");
+        filteredData.forEach((filteredUfoReport) => {
+          var row = tbody.append("tr");
+          Object.entries(filteredUfoReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+          });
+        });
+      }
+      else{
+      tbody.html("");
+      console.log("Nothing");
+      tbody.append("td").text("Information not available for this date!");
+      }
+});
+  
